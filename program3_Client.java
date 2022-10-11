985c81e1c23ba9d7d06e4018e0d0862c9a8b7902
Onkar More <93419853+OnkarMore3489@users.noreply.github.com>
2022-10-11 09:05:25 +0530

    Add files via upload

---
+++ program3_Client.java
@@ -0,0 +1,51 @@
+/*
+Write a program for an online test. The server randomly selects 10 questions from a database 
+and send them to the client one by one. Each question is sent after the answer to the 
+previous question is received. The server sends the score at the end of the exam.
+ */
+import java.io.*;
+import java.net.*;
+
+class program3_Client
+{
+  public static void main(String args[])throws Exception
+  {
+   String str1,str2;
+
+   System.out.println("Client is running....");
+
+   Socket s = new Socket("localhost",2101);
+
+   PrintStream ps = new PrintStream(s.getOutputStream());
+
+   BufferedReader br2 = new BufferedReader(new InputStreamReader(s.getInputStream()));
+
+   BufferedReader br1 = new BufferedReader(new InputStreamReader(System.in));
+
+   int i=1;
+   while(i!=10)
+   {
+    str1=br2.readLine();
+    str2=br2.readLine();
+    System.out.println(str1);
+    String Arr[]=str2.split(" ");
+    System.out.println(Arr[0]);
+    System.out.println(Arr[1]);
+    System.out.println(Arr[2]);
+    System.out.println(Arr[3]);
+
+    String res;
+    System.out.println("Enter Your Choice : ");
+    res=br1.readLine();
+    ps.println(res);
+    i++;
+   }
+   System.out.println("Your Score is : "+br2.readLine());
+
+   s.close();
+   ps.close();
+   br1.close();
+   br2.close();
+  }
+}
+
